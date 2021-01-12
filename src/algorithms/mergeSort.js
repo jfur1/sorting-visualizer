9cc29f9093215531e13d9df550e083730266293b
John Furlong <johnfurlong24@gmail.com>
2021-01-12 14:24:36 -0700

    Restructured to support react hooks/refs. Starting mergesort implementation

---
+++ src/algorithms/mergeSort.js
@@ -0,0 +1,5 @@
+export function mergesort(arr){
+    const copy = [...arr];
+    const len = copy.length;
+
+}
--- src/sortingVisualizer/sortingVisualizer.jsx
+++ src/sortingVisualizer/sortingVisualizer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './sortingVisualizer.css';
 
 // Change this value for the speed of the animations.
@@ -7,59 +7,124 @@ const ANIMATION_SPEED_MS = 1;
 // Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 100;
 
-export default class SortingVisualizer extends React.Component{
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            array: [],
-            animationSpeedMs: 1,
-            nBars: NUMBER_OF_ARRAY_BARS,
-            isSorted: false,
-            isRunning: false,
-        };
+export default function SortingVisualizer(props){
+    // Use hooks to add react-states to function components without creating new classes
+    // Prototype:
+    //      const [foo, bar] = useState(x)
+    //      where foo is the current state and bar is the function that updates it
+    //      argument x cooresponds to the initial state
+    const [array, setArray] = useState([]);
+    const [isRunning, setIsRunning] = useState(false);
+    const [isSorted, setIsSorted] = useState(false);
+    // Use ref to store array bars for animating
+    const containerRef = useRef(null);
+
+    // Exectued after each render (refresh page | new-array button)
+    useEffect(initArray, []);
+
+    function initArray(){
+        if(isRunning) return;
+        if(isSorted) resetArrayColor();
+        setIsSorted(false);
+        const arr = [];
+        for(let i = 0; i < NUMBER_OF_ARRAY_BARS; i++){
+            // Starting from value 5 so bars will be visible
+            arr.push(randomIntFromInterval(5, 150));
+        }
+        setArray(arr);
     }
 
-        componentDidMount(){
-            this.resetArray();
+    function resetArrayColor(){
+        const arrayBars = containerRef.current.children;
+        for(let i = 0; i < array.length; i++){
+            const arrayBarStyle = arrayBars[i].style;
+            arrayBarStyle.backgroundColor = '';
         }
+    }
 
-        resetArray(){
-            const arr = [];
-            for(let i = 0; i < this.state.nBars; i++){
-                // Starting from value 5 so bars will be visible
-                arr.push(randomIntFromInterval(5, 150));
-            }
-            this.setState({array: arr});
-            console.log(arr)
-;        }
-
-    render(){
-        const {array} = this.state;
-        
-        return (
-            <div className="app-container">
-                <div className = "array-container">
-                    {array.map((value, idx) => (
-                        <div
-                            className="array-bar"
-                            key={idx}
-                            style={{
-                                // Value for height is half of the array value
-                                height: `${value * 0.5 }vmin`,
-                                // Width of one bar is 1% of all the bars' widths 
-                                width: `${100 / NUMBER_OF_ARRAY_BARS}vw`,
-                            }}></div>
-                    ))}
-                
-                </div>
-            </div>
+    function animateSortedArray(){
+        const arrayBars = containerRef.current.children;
+        for(let i = 0; i < arrayBars.length; i++){
+            const arrayBarStyle = arrayBars[i].style;
+            setTimeout(
+                () => (arrayBarStyle.backgroundColor = 'green'),
+                i * ANIMATION_SPEED_MS,
+            );
+        }
+        setTimeout(() => {
+            setIsSorted(true);
+            setIsRunning(false);
+        }, arrayBars.length * ANIMATION_SPEED_MS
         );
-    }  
-}
+    }
 
-// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-function randomIntFromInterval(min, max) {
-    // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    function animateBar(index){
+        const arrayBars = containerRef.current.children;
+        const arrayBarStyle = arrayBars[index].style;
+        setTimeout(() => {
+            arrayBarStyle.backgroundColor = 'turquoise';
+        }, ANIMATION_SPEED_MS);
+        setTimeout(() => {
+            arrayBarStyle.backgroundColor = '';
+        }, 2 * ANIMATION_SPEED_MS)
+    }
+
+    function animateArray(animations){
+        if(isRunning) return;
+        setIsRunning(true);
+        animations.forEach(([comparison, swapped], index) => {
+            setTimeout(() => {
+                if(!swapped){
+                    if(comparison.length === 2){
+                        const [i, j] = comparison;
+                        animateBar(i);
+                        animateBar(j);
+                    } else{
+                        const [i] = comparison;
+                        animateBar(i);
+                    }
+                } else{
+                    setArray((prevArr) =>{
+                        const [k, newVal] = comparison;
+                        const newArr = [...prevArr];
+                        newArr[k] = newVal;
+                        return newArr;
+                    });
+                }
+            },  index * ANIMATION_SPEED_MS);
+        });
+        setTimeout(() => {
+            animateSortedArray();
+        }, animations.length * ANIMATION_SPEED_MS);
+    }
+
+    // From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    function randomIntFromInterval(min, max) {
+        // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    return (
+        <div className="app-container">
+            <div className="navbar">
+                <button onClick={initArray}>New Array</button>
+                <button>Merge Sort</button>
+                <button>Quick Sort</button>
+            </div>
+            <div className = "array-container" ref={containerRef}>
+                {array.map((value, idx) => (
+                    <div
+                        className="array-bar"
+                        key={idx}
+                        style={{
+                            // Value for height is half of the array value
+                            height: `${value * 0.5 }vmin`,
+                            // Width of one bar is 1% of all the bars' widths 
+                            width: `${100 / NUMBER_OF_ARRAY_BARS}vw`,
+                        }}></div>
+                ))}
+            </div>
+            <footer id="footer"></footer>
+        </div>
+    );
 }
