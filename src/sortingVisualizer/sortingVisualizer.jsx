e8c467105942ddb73670c8f1d237660df6bdef08
John Furlong <johnfurlong24@gmail.com>
2021-01-12 13:12:44 -0700

    Cleaned up css so I can start testing sorting algos

--- src/sortingVisualizer/sortingVisualizer.jsx
+++ src/sortingVisualizer/sortingVisualizer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './sortingVisualizer.css';
 
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 100;
 
 export default class SortingVisualizer extends React.Component{
     
@@ -9,7 +14,9 @@ export default class SortingVisualizer extends React.Component{
         this.state = {
             array: [],
             animationSpeedMs: 1,
-            nBars: 300,
+            nBars: NUMBER_OF_ARRAY_BARS,
+            isSorted: false,
+            isRunning: false,
         };
     }
 
@@ -18,26 +25,34 @@ export default class SortingVisualizer extends React.Component{
         }
 
         resetArray(){
-            const array = [];
-            for(let i = 0; i < this.nBars; i++){
-                array.push(randomIntFromInterval(5, 700));
+            const arr = [];
+            for(let i = 0; i < this.state.nBars; i++){
+                // Starting from value 5 so bars will be visible
+                arr.push(randomIntFromInterval(5, 150));
             }
-            this.setState({array});
-        }
+            this.setState({array: arr});
+            console.log(arr)
+;        }
 
     render(){
         const {array} = this.state;
         
         return (
-            <div className = "array-container">
-                {array.map((value, idx) => (
-                    <div
-                        className="array-bar"
-                        key={idx}
-                        style={{
-                            height: `${value}px`,
-                        }}></div>
-                ))}
+            <div className="app-container">
+                <div className = "array-container">
+                    {array.map((value, idx) => (
+                        <div
+                            className="array-bar"
+                            key={idx}
+                            style={{
+                                // Value for height is half of the array value
+                                height: `${value * 0.5 }vmin`,
+                                // Width of one bar is 1% of all the bars' widths 
+                                width: `${100 / NUMBER_OF_ARRAY_BARS}vw`,
+                            }}></div>
+                    ))}
+                
+                </div>
             </div>
         );
     }  
@@ -48,4 +63,3 @@ function randomIntFromInterval(min, max) {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-  
