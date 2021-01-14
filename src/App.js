2be20cf909393a75090cb4d16763d339c1ed875a
John Furlong <johnfurlong24@gmail.com>
2021-01-13 19:10:04 -0700

    Added titles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+      <head><title>Test</title></head>
       <SortingVisualizer></SortingVisualizer>
     </div>
   );
